b22798e1ca02b64f90599fff10108677da607559
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-05-24 07:23:12 +0530

    Add more text on description

--- src/components/descriptionanimation/descriptionanimation.js
+++ src/components/descriptionanimation/descriptionanimation.js
@@ -7,13 +7,18 @@ const Descriptionanimation =()=>{
     return(
         <div className="descriptioncontainer">
             <h1>
-            <p>Full-Stack web developer</p>
-            <span> As a highly driven and enthusiastic IT professional, I seek an internship
- opportunity within a reputable organization. My dedication to personal and
- professional growth fuels my ambition to gain practical experience. I thrive on
- challenges and believe my skills and determination would make me a valuable
- contributor to your team. Through a software engineering internship, I aim to
- refine my abilities and propel my career in IT to the next level.</span>
+            <p style={{color:"#3586ff", fontSize:"20px", gap:"10px"}}>Full-Stack web developer</p>
+            <span> I am a dedicated and passionate IT professional committed to continuous learning and growth. 
+                    I love tackling challenges and strive for excellence in every project. 
+                    With a strong background in software engineering and a keen interest in new technologies, 
+                    I am confident in my ability to contribute effectively to any team.
+                    Throughout my career, I've actively pursued opportunities to expand my skills and knowledge, 
+                    whether through hands-on projects, teamwork, or independent learning. 
+                    This approach has given me a diverse skill set and the ability to adapt to various technical environments.
+
+                    I excel at solving complex problems, developing innovative solutions, and improving processes to enhance efficiency. 
+                    My goal is to use my skills and passion for technology to achieve meaningful results and advance my career in IT. 
+                    I look forward to contributing to dynamic and forward-thinking organizations where I can continue to grow both personally and professionally.</span>
             </h1>
         </div>
     );
