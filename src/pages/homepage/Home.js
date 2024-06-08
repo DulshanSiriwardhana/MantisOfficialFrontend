40d4b7e7875df4a0e1e7d032d0120a77cc37eaae
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-06-08 22:52:54 +0530

    add recent projects

--- src/pages/homepage/Home.js
+++ src/pages/homepage/Home.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
 import Nameanimation from '../../components/nameanimation/nameanimation';
 import Dp from '../../components/dp/dp';
 import Descriptionanimation from '../../components/descriptionanimation/descriptionanimation';
+import axios from 'axios';
+import Article from '../../components/article/article';
 
 const Home = () =>{
+    const [articles, setArticles] = useState(null);
+    var i=0;
+    useEffect(()=>{
+        getArticles();
+    },[]);
+
+    const getArticles =async()=>{
+        const res = await axios.get('https://mantisofficialbackend.onrender.com/articles/');
+        setArticles(res);
+    }
     return(
-        <div className='homecontainer'>
-            <div className='hometop'>
-                <div className='homeleft'>
-                    <div className='nameanimation'>
-                        <Nameanimation/>
+        <div className='home'>
+            <div className='homecontainer'>
+                <div className='hometop'>
+                    <div className='homeleft'>
+                        <div className='nameanimation'>
+                            <Nameanimation/>
+                        </div>
+                        <div className='description'>
+                            <Descriptionanimation/>
+                        </div>
                     </div>
-                    <div className='description'>
-                        <Descriptionanimation/>
+                    <div className='homeright'>
+                        <Dp/>
                     </div>
                 </div>
-                <div className='homeright'>
-                    <Dp/>
+                <div className='homebottum'>
+                    <p>Recent Projects</p>
+                    <div className="articlepagecontainer1">
+                        {<div className="articlepagecontainer2">
+                            {articles?(articles.data.map((line,i=0)=>(
+                                <div className="articlepagecontainer" key={line._id}>
+                                    {i++<2?<Article articleId={line._id}/>:null}
+                                </div>
+                            ))):null}
+                        </div>}
+                    </div>
                 </div>
             </div>
         </div>
